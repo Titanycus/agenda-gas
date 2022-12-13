a83662c4a72b1f74d6f5fc89b31ce6ed939865b8
Titanycus <japadillamarcano@gmail.com>
2022-12-13 17:23:40 -0400

    video45 curso gas-avanzado

--- funciones.js
+++ funciones.js
@@ -7,3 +7,10 @@ function obtenerDatosHTML(nombre)
 {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent(); 
 }
+
+function obtenerContactos()
+{
+    let hoja = SpreadsheetApp.openById('1GTqZkfUXAsgNd7FDWxk82CL_mz5fEhUYMCJWymrv3LI').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+}
