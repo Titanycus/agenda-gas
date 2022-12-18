7666fad78c87acfa34db560fd851fb36fc79f1be
Titanycus <japadillamarcano@gmail.com>
2022-12-18 18:33:52 -0400

    video59 curso-gas-avanzado

--- funciones.js
+++ funciones.js
@@ -39,7 +39,7 @@ function modificarContacto(numFila, datos)
 
 function importarContactos()
 {
-    let url = 'https://randomuser.me/api/?results=5&inc=name,email,phone';
+    let url = 'https://randomuser.me/api/?results=5&inc=name,email,phone,picture';
     let respuesta = UrlFetchApp.fetch(url).getContentText();
 
     let datos = JSON.parse(respuesta);
@@ -48,5 +48,5 @@ function importarContactos()
 
 function insertarContactoJSON(contacto)
 {
-  HOJA.appendRow([contacto.name.first,contacto.name.last,contacto.email,contacto.phone]);
+  HOJA.appendRow([contacto.name.first,contacto.name.last,contacto.email,contacto.phone,contacto.picture.medium]);
 }
